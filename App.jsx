350a6a27051ea885511a4f24784ad19a472aa65c
Isha Hasham <144732430+ishahasham@users.noreply.github.com>
2024-09-26 23:44:39 +0500

    react native boiler plate

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+        <View>
+        <Text style={{fontSize:50}}>hello world</Text>
+      </View>
+
+  )
+}
+
+export default App
